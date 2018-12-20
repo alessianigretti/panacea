ae5bf9f85c586628a7bb39121a184879543e016e
unknown <alessia.nigretti@gmail.com>
2018-12-20 22:59:02 +0000

    Added camera to scene 2

---
+++ BiofeedbackVR/Assets/AutoDolly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class AutoDolly : MonoBehaviour
+{
+    public float m_DurationInSeconds = 20f;
+
+    private CinemachineTrackedDolly dolly;
+    private float startTime, elapsedTime, counter;
+
+    void Awake()
+    {
+        dolly = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>();
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        elapsedTime = Time.time - startTime;
+
+        if (elapsedTime <= m_DurationInSeconds)
+        {
+            counter = Mathf.Lerp(0, 1, elapsedTime / m_DurationInSeconds);
+        }
+
+        dolly.m_PathPosition = counter;
+    }
+}
