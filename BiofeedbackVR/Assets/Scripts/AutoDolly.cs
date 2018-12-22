b612a99f5971c84757e14ea4089849bfa6efe63d
unknown <alessia.nigretti@gmail.com>
2018-12-22 14:10:53 +0000

    Added water

--- BiofeedbackVR/Assets/Scripts/AutoDolly.cs
+++ BiofeedbackVR/Assets/Scripts/AutoDolly.cs
@@ -6,13 +6,17 @@ using Cinemachine;
 public class AutoDolly : MonoBehaviour
 {
     public float m_DurationInSeconds = 20f;
+    public CinemachineSmoothPath m_DollyPath;
+    public int m_LastWaypoint;
 
     private CinemachineTrackedDolly dolly;
+    private int lastWayPoint;
     private float startTime, elapsedTime, counter;
 
     void Awake()
     {
         dolly = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>();
+        lastWayPoint = m_DollyPath.m_Waypoints[m_DollyPath.m_Waypoints.GetLength()];
         startTime = Time.time;
     }
 
@@ -22,7 +26,7 @@ public class AutoDolly : MonoBehaviour
 
         if (elapsedTime <= m_DurationInSeconds)
         {
-            counter = Mathf.Lerp(0, 1, elapsedTime / m_DurationInSeconds);
+            counter = Mathf.Lerp(0, m_LastWaypoint, elapsedTime / m_DurationInSeconds);
         }
 
         dolly.m_PathPosition = counter;
